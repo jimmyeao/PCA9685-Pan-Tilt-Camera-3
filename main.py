bc1cac9ba177ae7caff2ea9587ba4984bd17c5f3
Jimmy White <jimmyeao@gmail.com>
2023-02-08 18:16:08 +0000

    added files

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-from flask import Flask, request
-from flask import Flask, redirect, url_for
+from flask import Flask, request, redirect, url_for, send_file
+from flask import Response
 from PCA9685 import PCA9685
 import time
-
+from PIL import Image
+import io
+from io import BytesIO
+from picamera2 import Picamera2
 app = Flask(__name__)
 
 pwm = PCA9685()
@@ -10,81 +13,34 @@ pwm.setPWMFreq(50)
 x,y = 90,90
 MAX_ANGLE = 180
 MIN_ANGLE = 50
+
+
+
+
+        
 @app.route("/")
 def index():
-    return '''
-    <html>
-        <head>
-            <style>
-                .btn {
-                    width: 80px;
-                    height: 40px;
-                    font-size: 20px;
-                    margin: 10px;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>Pan Tilt Control</h1>
-            <button class="btn" id="left">Left</button>
-            <button class="btn" id="right">Right</button>
-            <button class="btn" id="up">Up</button>
-            <button class="btn" id="down">Down</button>
-            <button class="btn" id="home">Home</button>
-            <script>
-                const left = document.getElementById("left");
-                const right = document.getElementById("right");
-                const up = document.getElementById("up");
-                const down = document.getElementById("down");
-                let x = 90;
-                let y = 90;
-                
-                left.addEventListener("mousedown", () => {
-                    const interval = setInterval(() => {
-                        if (x > 0) {
-                            x -= 1;
-                            fetch("/left", {method: "POST"});
-                        }
-                    }, 50);
-                    left.addEventListener("mouseup", () => clearInterval(interval));
-                });
-                right.addEventListener("mousedown", () => {
-                    const interval = setInterval(() => {
-                        if (x < 180) {
-                            x += 1;
-                            fetch("/right", {method: "POST"});
-                        }
-                    }, 50);
-                    right.addEventListener("mouseup", () => clearInterval(interval));
-                });
-                up.addEventListener("mousedown", () => {
-                    const interval = setInterval(() => {
-                        if (y < 180) {
-                            y += 1;
-                            fetch("/up", {method: "POST"});
-                        }
-                    }, 50);
-                    up.addEventListener("mouseup", () => clearInterval(interval));
-                });
-                down.addEventListener("mousedown", () => {
-                    const interval = setInterval(() => {
-                        if (y > 0) {
-                            y -= 1;
-                            fetch("/down", {method: "POST"});
-                        }
-                    }, 50);
-                    down.addEventListener("mouseup", () => clearInterval(interval));
-                });
-            </script>
-        </body>
-    </html>
-    '''
+    picam2 = Picamera2() 
+    picam2.start() 
+    time.sleep(1)
+    image = picam2.capture_image("main")
+    byte_io = BytesIO()
+    image.save(byte_io, format='png')
+    byte_io.seek(0)
+    picam2.stop() 
+    return send_file(byte_io, mimetype='image/png')
+
+
+   
+@app.route("/start_home", methods=["POST"])
 def starthome():
     global x, y
     x = 90
     y = 90
     pwm.setRotationAngle(0, x)
     pwm.setRotationAngle(1, y)
+    return "Home position set"
+
 @app.route("/up", methods=["POST"])
 def move_up():
     global x
@@ -131,4 +87,5 @@ def home():
 
 if __name__ == '__main__':
     starthome()
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True,host='0.0.0.0', port=80)
+
